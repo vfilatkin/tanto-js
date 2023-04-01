18fcfee9f1dfeaf23771c35cd8bcf15c0bc2b721
vfilatkin <gector289@gmail.com>
2023-04-01 22:04:05 +0300

    + effects

--- examples/delayExample.js
+++ examples/delayExample.js
@@ -4,23 +4,33 @@ const delay = fn => {
   }, 2000)
 }
 
-const IntervalCounter = () => {
-  let [count, setCount] = t.state(0);
+const IntervalCounter = initialValue => {
+  let [count, setCount] = t.state(initialValue);
   delay(()=>{setCount(++count)})
+
+  t.effect(()=>{
+    console.log('Initial value ' + initialValue);
+  },[]);
+  
+  t.effect(()=>{
+    document.title = 'The count is ' + count;
+  }, [count]);
+
   return (t('span'),t.text(`Count: ${count}`),t())
+
 }
 
 const App = title => {
   return (
     t('div'),
       t.text(title),t.void('br'),
-      t(IntervalCounter),
+      t(IntervalCounter, 0),
     t()
   );
 }
 
 t.ready(() => {
   t.patch(document.getElementById('app'), () => {
-    t(App, 'This is async table example')
+    t(App, 'This is a timeout counter example')
   })
 })
