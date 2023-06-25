da27b774ece7e897f0182999f47bfd9f68369eac
vfilatkin <gector289@gmail.com>
2023-06-25 16:57:28 +0300

    + bundle minifcation

--- utlis/hs.converter.js
+++ utlis/hs.converter.js
@@ -1,7 +1,16 @@
 let HSConverter = (function () {
   let config = {
-    minifyReferences: true,
+    minifyRendererReferences: true,
     minifyFragments: false,
+    minifyReferences: false,
+  }
+
+  const RENDERER_REFERENCES = {
+    T: 't',
+    ATTRIBUTE: 't.attr',
+    TEXT: 't.text',
+    COMMENT: 't.comment',
+    LISTENER: 't.on',
   }
 
   function CodeFormatter(tabSpaces) {
@@ -31,14 +40,14 @@ let HSConverter = (function () {
       return this;
     }
 
-    this.render = function () {
+    this.render = function (minify) {
       let text = '';
       for (let lI = 0, tL = this.text.length; lI < tL; lI++) {
         let line = this.text[lI];
         if (line instanceof CodeFormatter) {
           text += line.render();
         } else {
-          if (config.minifyFragments) {
+          if (minify || config.minifyFragments) {
             text += line.text ? line.text : '';
           } else {
             let spaces = ' '.repeat(this.tabSpaces).repeat(this.tabLevel + line.tabs)
@@ -48,39 +57,10 @@ let HSConverter = (function () {
       }
       return text;
     }
-
-  }
-
-  function numToChar(n) {
-    const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    let
-      cL = chars.length,
-      o = n - 1,
-      letters = chars[o % cL];
-    while ((o = Math.floor(o / cL)) > 0) {
-      letters = chars[--o % cL] + letters;
-    }
-    return letters;
-  }
-
-  function generateNamespace(length) {
-    let names = [];
-
-    for (let i = 0; i < length; i++) {
-      names.push(numToChar(i + 1));
-    }
-
-    return names;
-  }
-
-  let refernces = {
-    attributeMethodName: { dev: 't.attr', min: 'a' },
-    textMethodName: { dev: 't.text', min: 'T' },
-    commentMethodName: { dev: 't.comment', min: 'c' },
-    eventListenerMethodName: { dev: 't.on', min: 'o' },
   }
 
   function toVNode(rootNode) {
+
     let DOMIndex = 0;
 
     const metaData = {
@@ -91,11 +71,9 @@ let HSConverter = (function () {
 
     function VNode(node) {
       let
-        tag = node.tagName ? node.tagName.toLowerCase(): null,
+        tag = node.tagName ? logKey('tagMap', `'${node.tagName.toLowerCase()}'`): null,
         [namespace, attributes, hsProps] = getVNodeAttributes(node);
-
-      logKey('tagMap', tag);
-
+      
       return {
         index: DOMIndex++,
         type: node.nodeType,
@@ -112,16 +90,20 @@ let HSConverter = (function () {
       if (metaData[map][key]) {
         metaData[map][key]++;
       } else {
-        metaData[map][key] = 1;
+        metaData[map][key] = 1
       }
+      return key
     }
     
     function logAttribute(attribute) {
-      logKey('attributeMap', attribute.name);
-      logKey('valueMap', attribute.value);
+      return { 
+        name: logKey('attributeMap', `'${attribute.name}'`), 
+        value: logKey('valueMap', `'${attribute.value}'`) 
+      }
     }
 
     function getVNodeAttributes(node) {
+
       let
         namespace,
         attributes = [],
@@ -142,13 +124,11 @@ let HSConverter = (function () {
                 hsProps[attribute.name] = attribute.value;
                 break;
               default:
-                logAttribute(attribute);
-                attributes.push({ name: attribute.name, value: attribute.value });
+                attributes.push(logAttribute(attribute));
             }
           }
         }
       }
-
       return [namespace, attributes, hsProps];
     }
 
@@ -156,52 +136,94 @@ let HSConverter = (function () {
       let
         children = [],
         cL;
-
       if (node.childNodes && (cL = node.childNodes.length)) {
         for (let cI = 0; cI < cL; cI++) {
           children.push(VNode(node.childNodes[cI]));
         }
       }
-
       return children;
     }
 
+    metaData[0] = {
+      [RENDERER_REFERENCES.T]: 2,
+      [RENDERER_REFERENCES.ATTRIBUTE]: 2,
+      [RENDERER_REFERENCES.TEXT]: 2,
+      [RENDERER_REFERENCES.COMMENT]: 2,
+      [RENDERER_REFERENCES.LISTENER]: 2
+    }
+    
     return [VNode(rootNode), metaData];
+
   }
 
+  function numToChar(n) {
+    const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let
+      cL = chars.length,
+      o = n - 1,
+      letters = chars[o % cL];
+    while ((o = Math.floor(o / cL)) > 0) {
+      letters = chars[--o % cL] + letters;
+    }
+    return letters;
+  }
 
-  function renderFragments(rootVNode, meta) {
+  function renderKeySpace(metaData){
+    let 
+      index = 0,
+      keys = []
+      values = [];
+    for(let mapKey in metaData){
+      const map = metaData[mapKey];
+      for(let key in map){
+        const string = map[key];
+        if(string > 1){
+          index++;
+          keys.push(map[key] = numToChar(index));
+          values.push(key);
+        } else {
+          map[key] = `${key}`;
+        }
+      }
+    }
+
+    let keySpace = new CodeFormatter()
+      .tab()
+      .line(`let [${keys.join()}]=[${values.join()}];`)
+      .line()
+    console.log(keySpace.render());
+  }
+
+  function renderFragments(rootVNode, metaData) {
+
     let currentFragment = new CodeFormatter();
     const FRAGMENTS = { __root__: currentFragment };
 
     function renderNode(vnode, root) {
       let pFragment = currentFragment;
-
       if (vnode.hsProps.fragment) {
         currentFragment.line(vnode.hsProps.fragment + '(),')
         currentFragment = FRAGMENTS[vnode.hsProps.fragment] = new CodeFormatter();
         root = true;
       }
-
       switch (vnode.type) {
         case 1:
           if (vnode.children.length === 0) {
-            currentFragment.line(`t('${vnode.tag}'${vnode.namespace ? `,'${vnode.namespace}'` : ''}),${renderNodeAttributes(vnode.attributes)}t(),`);
+            currentFragment.line(`${metaData[0][RENDERER_REFERENCES.T]}(${metaData.tagMap[vnode.tag]}${vnode.namespace ? `,${metaData.valueMap[vnode.namespace]}` : ''}),${renderNodeAttributes(vnode.attributes)}${metaData[0][RENDERER_REFERENCES.T]}(),`);
           } else {
-            currentFragment.line(`t('${vnode.tag}'${vnode.namespace ? `,'${vnode.namespace}'` : ''}),${renderNodeAttributes(vnode.attributes)}`).tab();
+            currentFragment.line(`${metaData[0][RENDERER_REFERENCES.T]}(${metaData.tagMap[vnode.tag]}${vnode.namespace ? `,${metaData.valueMap[vnode.namespace]}` : ''}),${renderNodeAttributes(vnode.attributes)}`).tab();
             renderNodeChildren(vnode.children);
             currentFragment.untab()
-              .line(`t()${root ? '' : ','}`);
+              .line(`${metaData[0][RENDERER_REFERENCES.T]}()${root ? '' : ','}`);
           }
           break;
         case 3:
-          currentFragment.line(`${config.minifyReferences ? refernces.textMethodName.min : refernces.textMethodName.dev}\`${vnode.content}\``);
+          currentFragment.line(`${metaData[0][RENDERER_REFERENCES.TEXT]}\`${vnode.content}\``);
           break;
         case 8:
-          currentFragment.line(`${config.minifyReferences ? refernces.commentMethodName.min : refernces.commentMethodName.dev}\`${vnode.content}\``);
+          currentFragment.line(`${metaData[0][RENDERER_REFERENCES.COMMENT]}\`${vnode.content}\``);
           break;
       }
-
       currentFragment = pFragment;
     }
 
@@ -214,12 +236,10 @@ let HSConverter = (function () {
 
     function renderNodeAttributes(attributes) {
       let text = '';
-
       for (let aI = 0, aL = attributes.length; aI < aL; aI++) {
         const attribute = attributes[aI];
-        text += `${config.minifyReferences ? refernces.attributeMethodName.min : refernces.attributeMethodName.dev}('${attribute.name}','${attribute.value}'),`
+        text += `${metaData[0][RENDERER_REFERENCES.ATTRIBUTE]}(${metaData.attributeMap[attribute.name]},${metaData.valueMap[attribute.value]}),`
       }
-
       return text;
     }
 
@@ -238,27 +258,14 @@ let HSConverter = (function () {
 
   function toRootNode(data) {
     let element;
-
     if (typeof data === 'string') {
       element = document.createElement('div');
       element.innerHTML = cleanHtml(data);
       return element.firstElementChild;
     }
-
     element = data.cloneNode(true);
     element.innerHTML = cleanHtml(element.innerHTML);
-
     return element;
-  }
-
-  function minifyRendererReferences() {
-    let methods = [];
-
-    for (const method in refernces) {
-      methods.push(`${refernces[method].min}=${refernces[method].dev}`);
-    }
-
-    return 'let ' + methods.join(',') + ';'
   }
 
   function renderFragment(name, code) {
@@ -274,36 +281,31 @@ let HSConverter = (function () {
       .line(')')
       .untab()
       .line('};');
-
     return fragment;
   }
-
 
   function renderBundle(data) {
     /* Get first node from text or element. */
     let
       rootNode = toRootNode(data),
-      [rootVNode, metaData] = toVNode(rootNode),
-      fragments = renderFragments(rootVNode);
+      [rootVNode, metaData] = toVNode(rootNode);
+    renderKeySpace(metaData);
+    let
+      fragments = renderFragments(rootVNode, metaData);
     console.log(metaData);
-
     delete fragments.__root__
-
     /* Create bunlde module. */
     let module = new CodeFormatter()
       .line(`let ${Object.keys(fragments).join()};`)
       .line('(function(){')
       .tab()
-      .line(minifyRendererReferences());
-
+    
     for (let block in fragments) {
       module.append(renderFragment(block, fragments[block]))
     }
-
     module
       .untab()
-      .line('})();');
-
+      .line('})();')
     return module.render();
   }
 
